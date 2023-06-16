1f7f5f22b9b7b9b05c0e600855abb0fac7743509
Umesh Bhatt <103458788+me-hem@users.noreply.github.com>
2023-06-16 20:53:05 +0530

    Update mining.py

--- mining.py
+++ mining.py
@@ -7,8 +7,9 @@ url = "http://127.0.0.1:" + port + "/mine"
 
 print("Mining started ")
 count = 0
+time.sleep(1)
 while count < 3:
-    time.sleep(10)
+    #time.sleep(10)
     res = req.get(url)
     print(res.content.decode('utf-8'))
     if res.content.decode('utf-8') == "No transactions to mine":
